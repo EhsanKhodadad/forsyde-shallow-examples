c3fb11af500fae715c6046a93787188a73ef47ff
Ehsan Khodadad <ehkh@dtu.dk>
2025-06-06 16:10:16 +0200

    dsp and adas

--- src/ForSyDe/Shallow/Example/Synchronous/dsp.hs
+++ src/ForSyDe/Shallow/Example/Synchronous/dsp.hs
@@ -14,12 +14,10 @@ windowSY winSize = mooreSY nextState output initState where
     output (currWin, _) = currWin
 
 dspSMA :: Int -> Signal Double -> Signal Double
-dspSMA n input = mapSY (\xs -> sum xs / fromIntegral n) 
-                 (windowSY n 1 input)
+dspSMA n input = mapSY (\xs -> sum xs / fromIntegral n) (windowSY n input)
 
 dspRMS :: Int -> Signal Double -> Signal Double
-dspRMS n input = mapSY(\xs->sqrt (sum(map(^2) xs) / fromIntegral n)) 
-                (windowSY n 1 input)
+dspRMS n input = mapSY(\xs->sqrt (sum(map(^2) xs) / fromIntegral n)) (windowSY n input)
 
 dspGain :: Double -> Signal Double -> Signal Double
 dspGain g = mapSY (* g) 
@@ -43,3 +41,43 @@ dspAudio gain n input = tagged where
     amplified  = dspGain gain rms        
     monitored  = dspMonitor amplified    
     tagged     = dspTag amplified monitored
+
+
+adjGain :: Double -> Double -> Double
+adjGain currentGain level
+    | level > 1.0  = max 0.0 (currentGain - 0.1) 
+    | level < -1.0 = min 5.0 (currentGain + 0.1) 
+    | otherwise    = currentGain -- No change
+
+dspFB :: Double -> Int -> Signal Double -> Signal (Double, Level)
+dspFB initGain n input = tagged where
+  filtered  = dspFilter input 
+  smoothed  = dspSMA n filtered 
+  rms       = dspRMS n smoothed 
+  adjusted  = mooreSY adjGain id initGain rms
+  amplified = zipWithSY (*) adjusted rms
+  monitored = dspMonitor amplified
+  tagged    = dspTag amplified monitored  
+
+-- This is a simple example of how to use the dspFB function with a signal.
+audio :: Signal Double
+audio = signal [1.4, -3.2, 0.1, 2.0, -1.5, 0.3, -0.7, 2.4, 0, -1.8, 
+                -0.3, 1.7, 1.9, 0.2, -3.1, -0.4, 0.6, 1.9, -2.1, 
+                -2.9, 0.5, 2.2, -0.2]
+gain :: Double
+gain = 2.5
+
+winSize :: Int
+winSize = 4
+
+-- >>> dspFilter audio
+
+-- >>> dspGain gain $ dspFilter audio
+
+-- >>> dspSMA winSize $ dspFilter audio
+
+-- >>> dspRMS winSize $ dspSMA winSize $ dspFilter audio
+
+-- >>> dspAudio gain winSize audio
+
+-- >>> dspFB gain winSize audio
